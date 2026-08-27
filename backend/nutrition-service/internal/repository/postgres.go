5f36778a70e012b074b8a8cb02ea5b93d4af9d48
kulon <dwhyb0@gmail.com>
2026-08-28 00:42:24 +0300

    feat(nutrition): реализовал GetDayPlan на уровне данных

--- backend/nutrition-service/internal/repository/postgres.go
+++ backend/nutrition-service/internal/repository/postgres.go
@@ -69,3 +69,77 @@ func (db *PostgresDB) SavePlan(ctx context.Context, plan service.SaveGeneratedPl
 
 	return tx.Commit(ctx)
 }
+
+func (db *PostgresDB) GetDayPlan(ctx context.Context, r service.GetDayPlanRequest) (service.GetDayPlanResponse, error) {
+	tx, err := db.pool.Begin(ctx)
+	if err != nil {
+		return service.GetDayPlanResponse{}, err
+	}
+	defer tx.Rollback(ctx)
+
+	var (
+		planTemplateID int32
+		waterGoal      int32
+	)
+
+	if err := tx.QueryRow(ctx, `
+	SELECT id, water_goal
+	FROM plan_templates
+	WHERE user_id = $1 AND is_active = true;
+	`, r.UserID).Scan(&planTemplateID, &waterGoal); err != nil {
+		return service.GetDayPlanResponse{}, err
+	}
+
+	var (
+		mealTemplateID int32
+		nutritionFacts service.NutritionFacts
+	)
+
+	if err := tx.QueryRow(ctx, `
+	SELECT id, calories, protein, fat, carb
+	FROM meal_templates
+	WHERE plan_id = $1;
+	`, planTemplateID,
+		nutritionFacts.Calories,
+		nutritionFacts.Protein,
+		nutritionFacts.Fat,
+		nutritionFacts.Carb).Scan(&mealTemplateID,
+		&nutritionFacts.Calories,
+		&nutritionFacts.Protein,
+		&nutritionFacts.Fat,
+		&nutritionFacts.Carb); err != nil {
+		return service.GetDayPlanResponse{}, err
+	}
+
+	meals := make([]service.MealItemsResponse, 0)
+
+	rows, err := tx.Query(ctx, `
+	SELECT id, name, recipe
+	FROM meal_items
+	WHERE meal_template_id = $1
+	`, mealTemplateID)
+	if err != nil {
+		return service.GetDayPlanResponse{}, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var meal service.MealItemsResponse
+
+		if err := rows.Scan(&meal.ID, &meal.Name, &meal.Recipe); err != nil {
+			return service.GetDayPlanResponse{}, err
+		}
+
+		meals = append(meals, meal)
+	}
+
+	if err := rows.Err(); err != nil {
+		return service.GetDayPlanResponse{}, err
+	}
+
+	return service.GetDayPlanResponse{
+		MealItems:      meals,
+		NutritionFacts: nutritionFacts,
+		WaterGoalMl:    waterGoal,
+	}, nil
+}
